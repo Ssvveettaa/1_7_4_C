79b666488d95d17e3093fe34cb1b672094bfb17f
Svetlana Panina <paninasi@inbox.ru>
2023-08-06 09:45:54 +0300

    Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 28:
+// Напишите программу, которая
+// принимает на вход число N
+// и выдаёт произведение чисел от 1 до N.
+// 4 –> 24
+// 5 –> 120
+
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = Factorial(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {result}");
+
+int Factorial(int num)
+{
+    int factorial = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked { factorial *= i; }
+    }
+    return factorial;
+}
